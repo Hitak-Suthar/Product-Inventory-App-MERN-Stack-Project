7f818b92ce3ae00fd40ebd619694799f645c8393
Hitak Suthar <hitaksuthar9680@gmail.com>
2025-12-09 17:56:38 +0530

    Frontend Added

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div>
+        <h4>This is my Footer</h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./home.css";
+
+function Home() {
+  return (
+    <div className='home-container my-4'>
+        <h2>Welcome to Product Inventory</h2><br/>
+        <p>This is my Home Component</p>
+    </div>
+  )
+}
+
+export default Home
