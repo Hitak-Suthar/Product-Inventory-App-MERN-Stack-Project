4fb621b825a479fe16e92d29ff1621a18c6bf7cd
Hitak Suthar <hitaksuthar9680@gmail.com>
2025-12-25 19:35:18 +0530

    normal changes

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -11,7 +11,7 @@ function Register({ setPage }) {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/register", {
+      const res = await fetch(`${API_URL}/api/users/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
